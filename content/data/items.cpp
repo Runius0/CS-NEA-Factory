40673efef1db54e4a665c491afc775bfb4e47c80
Runius0 <139635965+Runius0@users.noreply.github.com>
2026-09-03 23:56:36 +0100

    splitter icon fix

--- content/data/items.cpp
+++ content/data/items.cpp
@@ -11,7 +11,7 @@ void loadItems() {
 	registerItem(new MachineItem(MACHINE_EXPORTER, (char*)"EXPORTER", { SPRITE_SIZE * 2, 0, SPRITE_SIZE, SPRITE_SIZE }, 50));
 	registerItem(new MachineItem(MACHINE_IMPORTER, (char*)"IMPORTER", { SPRITE_SIZE * 1, 0, SPRITE_SIZE, SPRITE_SIZE}, 50));
 	registerItem(new MachineItem(MACHINE_CRATE, (char*)"CRATE", { SPRITE_SIZE, SPRITE_SIZE, SPRITE_SIZE, SPRITE_SIZE }, 50));
-	registerItem(new MachineItem(MACHINE_SPLITTER, (char*)"SPLITTER", { SPRITE_SIZE, SPRITE_SIZE, SPRITE_SIZE, SPRITE_SIZE }, 50));
+	registerItem(new MachineItem(MACHINE_SPLITTER, (char*)"SPLITTER", { 0, SPRITE_SIZE, SPRITE_SIZE, SPRITE_SIZE }, 50));
 	registerItem(new Item((char*)"RAW TEST", { SPRITE_SIZE * 3, 0, SPRITE_SIZE, SPRITE_SIZE }, 50));
 	registerItem(new Item((char*)"COOKED TEST", { SPRITE_SIZE * 4, 0, SPRITE_SIZE, SPRITE_SIZE }, 50));
 }
